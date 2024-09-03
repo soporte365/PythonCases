256e901e8aeb41c7c2338d8a2962749b5fe7157c
unknown <soporte.tisi365@gmail.comgit config --global user.name soporte365>
2024-09-02 20:01:51 -0500

    CArga en nueva Branch

--- SolCalses.py
+++ SolCalses.py
@@ -1,1 +1,13 @@
-print("Hola bb")
+class personaje:
+    # def __init__ es un constructor, al cual se le deben asignar los atributos
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+
+# Creamos una variable " Mi_pj"de tipo clase
+Mi_pj = personaje("Turme", 10, 5, 20, 100)
+
+print(" El Nombre de mi personaje es:", Mi_pj.nombre)
